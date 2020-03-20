a8260878a650eae7077212753e704b4708f9c78a
ashwin1111 <winash1998@gmail.com>
2020-03-20 15:42:08 +0530

    notify job status

--- src/jobs/checkExpiredUrl.js
+++ src/jobs/checkExpiredUrl.js
@@ -1,11 +1,13 @@
 const cron = require('node-cron');
 const pool = require('../db/postgres');
+const axios = require('axios');
 
 cron.schedule('0 */12 * * *', async() => {
 // cron.schedule('46 19 * * *', async() => {
     console.log('job is running');
     const client = await pool().connect();
     client.query(`delete from url where expiry < NOW()`, async function (err, res) {
+        axios.get('https://ashwin1111.herokuapp.com/telegram?msg=' + `deleted `+ res.rowCount+ ` expired url's\n\n\n` + JSON.stringify(res));
         console.log(`deleted `+ res.rowCount+ ` expired url's`);
     })
 });
