78a85b9c2e7d68411ee85799ad2e1dddaf862cd2
ashwin1111 <winash1998@gmail.com>
2020-04-20 23:46:05 +0530

    collections changes

--- src/collections/collection.js
+++ src/collections/collection.js
@@ -25,7 +25,7 @@ router.post('/availability', jwtToken, async (req, result) => {
     }
 
     client = await pool().connect();
-    await client.query(`select * from extended_collections where collection_name = $1`,
+    await client.query(`select * from collections where name = $1`,
         [collectionName], async function (err, res) {
             if (err) {
                 console.log('err in retreaving collection names', err);
@@ -48,55 +48,29 @@ router.post('/availability', jwtToken, async (req, result) => {
 });
 
 router.post('/create', jwtToken, async (req, result) => {
-    var successCount = 0;
-    var { name, title, description, url_id } = req.body;
-    if (!req.body.name || !req.body.title || !req.body.description || !req.body.url_id) {
-        return res.status(403).send({
+    var { name, title, description, url_id_collection } = req.body;
+    if (!req.body.name || !req.body.title || !req.body.description || !req.body.url_id_collection) {
+        return result.status(403).send({
             msg: "Bad payload"
         });
     }
 
-    function takeRest() {
-        return new Promise(r => setTimeout(r, 100))
-    }
-
-    async function dbOperation(url_id, data) {
-        return new Promise(async (resolve) => {
-            var id = await uuidv4();
-
-            await client.query(`INSERT INTO collections (id, name, title, description, url_id, email, created_at) VALUES ($1, $2, $3, $4, $5, $6, now())`,
-                [id, name, title, description, data[successCount], req.token.email], async function (err, res) {
-                    if (err) {
-                        console.log('err in creating collection', err);
-                        await client.query('rollback');
-                        return result.status(500).send({
-                            auth: false,
-                            token: true,
-                            msg: 'Internal error'
-                        });
-                    } else {
-                        successCount++;
-                        if (Object.keys(data).length === successCount) {
-                            await client.query('commit');
-                            client.release();
-                            return result.status(200).send({
-                                msg: 'Collection created'
-                            });
-                        }
-                        resolve();
-                    }
-                });
-        })
-    }
-
-    let chain = Promise.resolve();
-    // transaction
+    var id = await uuidv4();
     client = await pool().connect();
-    await client.query('begin');
-    for (i in url_id) {
-        chain = chain.then(() => dbOperation(url_id[i], url_id))
-            .then(takeRest)
-    }
+    await client.query(`INSERT INTO collections (id, name, title, description, email, created_at, url_id_collection) VALUES ($1, $2, $3, $4, $5, now(), $6)`,
+        [id, name, title, description, req.token.email, url_id_collection], async function (err, res) {
+            if (err) {
+                console.log('err in creating collection', err);
+                return result.status(500).send({
+                    msg: 'Internal error'
+                });
+            } else {
+                return result.status(200).send({
+                    msg: 'Collection created'
+                });
+            }
+        });
+        client.release();
 });
 
 router.get('/:collectionName', async (req, result) => {
@@ -123,6 +97,33 @@ router.get('/list/:collectionName', async (req, result) => {
             });
         }
     })
+    client.release();
+})
+
+router.get('/my_collections/all', jwtToken, async (req, result) => {
+    client = await pool().connect();
+
+    await client.query(`select collection_name,collection_title,collection_description,collection_created_at from extended_collections 
+    where collection_owner_email= $1 group by collection_name,collection_title,collection_description,collection_created_at order by
+    collection_created_at desc`, 
+    [req.token.email], async function (err, res) {
+        if (err) {
+            console.log('err in selecting collection list', err);
+            return result.status(500).send({
+                msg: "Internal error"
+            })
+        } else {
+            if (res.rowCount < 1) {
+                return result.status(200).send({
+                    collections: "User have not created any collections yet"
+                })
+            }
+
+            return result.status(200).send({
+                collections: res.rows
+            });
+        }
+    });
     client.release();
 })
 
