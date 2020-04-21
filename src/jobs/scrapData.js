684a54860968bf48e554f714374f03a4c6ba2646
ashwin1111 <winash1998@gmail.com>
2020-04-21 16:08:27 +0530

    minor corrections

--- src/jobs/scrapData.js
+++ src/jobs/scrapData.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 cron.schedule('0 */1 * * *', async() => {
     scrapData();
 });
-
+scrapData();
 async function scrapData () {
 
     var successCount = 0;
@@ -17,14 +17,15 @@ async function scrapData () {
     async function dbOperation(rows) {
         return new Promise(async (resolve) => {
             axios.get('https://webpage-details.herokuapp.com/get_page_details?web_address=' + rows[successCount].big_url).then(async(res) => {
+                var description = res.data.data.meta_data[0] || res.data.data.first_p_tag[0] || null;
                 await client.query(`update url set title = $1, description = $2, last_updated_at = now() where id = $3`,
-                [res.data.data.title[0], res.data.data.meta_data[0] || res.data.data.first_p_tag[0], rows[successCount].id], async function (err, res) {
+                [res.data.data.title[0], description, rows[successCount].id], async function (err, res2) {
                     if (err) {
                         console.log('err in updating scrap data', err);
                     } else {
                         successCount++;
                         console.log('updated 1 scraped data');
-                        axios.get('https://ashwin1111.herokuapp.com/telegram?msg=' + `updated scraped data` + res.data.data.title[0]);
+                        axios.get('https://ashwin1111.herokuapp.com/telegram?msg=' + `updated scraped data, titled ` + res.data.data.title[0]);
                         if (rows.length === successCount) {
                             client.release();
                         }
@@ -32,8 +33,9 @@ async function scrapData () {
                     }
                 });
             }).catch(err => {
-                successCount++;
                 console.log(`Thatha's API didnt answer for ` + rows[successCount].big_url + ' err ' + err);
+                axios.get('https://ashwin1111.herokuapp.com/telegram?msg=' + `Thatha's API didnt answer for ` + rows[successCount].big_url + ' err ' + err);
+                successCount++;
                 resolve();
             })
         })
@@ -47,7 +49,7 @@ async function scrapData () {
         }
     }
     client = await pool().connect();
-    client.query(`select * from url where description is null`, async function (err, res) {
+    client.query(`select * from url where title is null`, async function (err, res) {
         if (err) {
             console.log('err in retreaving url scrap jobs');
         } else if (res.rowCount < 1) {
